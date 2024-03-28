40003fb47679b67a53c1e53183c6d7079f9aab6e
emotek <emotek@pop-os.localdomain>
2024-03-27 23:22:46 -0300

    share page fix

--- src/app/(pages)/(home)/layout.tsx
+++ src/app/(pages)/(home)/layout.tsx
@@ -1,27 +1,23 @@
-'use client'
-import {useEffect} from 'react';
+"use client";
+import { useEffect } from "react";
 import Banner from "@/components/Banner";
 export default function HomeLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  useEffect(()=>{
-// Verificando se o usuário está acessando a página a partir do aplicativo do Instagram
-        if (window.navigator.userAgent.includes("Instagram")) {
-            // Redirecionando para o navegador nativo do Android (Google Chrome)
-            window.location.href = "intent://seusite.com#Intent;scheme=http;package=com.android.chrome;end";
-        }
-        // Verificando se o usuário está acessando a página a partir do aplicativo do WhatsApp
-        else if (window.navigator.userAgent.includes("WhatsApp")) {
-            // Redirecionando para o aplicativo do WhatsApp
-            window.location.href = "intent://seusite.com#Intent;scheme=http;package=com.android.chrome;end";
-        }
-        // Verificando se o usuário está acessando a página a partir do aplicativo do Facebook
-        else if (window.navigator.userAgent.includes("FBAV")) {
-            // Redirecionando para o aplicativo do Facebook
-            window.location.href = "intent://seusite.com#Intent;scheme=http;package=com.android.chrome;end";
-        }
+  useEffect(() => {
+  const userAgent = window.navigator.userAgent;
 
-  })
+    // Verificando se o usuário está acessando a página a partir do aplicativo do Instagram, WhatsApp ou Facebook
+    if (userAgent.includes("Instagram") || userAgent.includes("WhatsApp") || userAgent.includes("FBAV")) {
+        // Perguntar ao usuário se deseja abrir no navegador Chrome nativo
+        const confirmRedirect = confirm("Deseja abrir esta página no navegador Chrome nativo do Android?");
+        if (confirmRedirect) {
+            // Redirecionar para o navegador nativo do Android (Google Chrome)
+            window.location.href = "intent://seusite.com#Intent;scheme=http;package=com.android.chrome;end";
+        }
+      console.log("quack", userAgent)
+    }  });
+
   return (
     <>
       <Banner />
