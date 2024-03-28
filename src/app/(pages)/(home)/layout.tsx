aeb077e45644e78a293ba2189f3f24e5824af372
emotek <emotek@pop-os.localdomain>
2024-03-27 23:15:45 -0300

    share page navigation fixed

--- src/app/(pages)/(home)/layout.tsx
+++ src/app/(pages)/(home)/layout.tsx
@@ -1,7 +1,27 @@
+'use client'
+import {useEffect} from 'react';
 import Banner from "@/components/Banner";
 export default function HomeLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  useEffect(()=>{
+// Verificando se o usuário está acessando a página a partir do aplicativo do Instagram
+        if (window.navigator.userAgent.includes("Instagram")) {
+            // Redirecionando para o navegador nativo do Android (Google Chrome)
+            window.location.href = "intent://seusite.com#Intent;scheme=http;package=com.android.chrome;end";
+        }
+        // Verificando se o usuário está acessando a página a partir do aplicativo do WhatsApp
+        else if (window.navigator.userAgent.includes("WhatsApp")) {
+            // Redirecionando para o aplicativo do WhatsApp
+            window.location.href = "intent://seusite.com#Intent;scheme=http;package=com.android.chrome;end";
+        }
+        // Verificando se o usuário está acessando a página a partir do aplicativo do Facebook
+        else if (window.navigator.userAgent.includes("FBAV")) {
+            // Redirecionando para o aplicativo do Facebook
+            window.location.href = "intent://seusite.com#Intent;scheme=http;package=com.android.chrome;end";
+        }
+
+  })
   return (
     <>
       <Banner />
